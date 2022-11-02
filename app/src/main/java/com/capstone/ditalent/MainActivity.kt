80c37ce0496d7b339a4e75d3ec47c9fbaa182600
rezacahyono <rezacahyono1@gmail.com>
2022-11-02 16:23:10 +0700

    adding splash-screen
    Signed-off-by: rezacahyono <rezacahyono1@gmail.com>

--- app/src/main/java/com/capstone/ditalent/MainActivity.kt
+++ app/src/main/java/com/capstone/ditalent/MainActivity.kt
@@ -2,12 +2,15 @@ package com.capstone.ditalent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.capstone.ditalent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
